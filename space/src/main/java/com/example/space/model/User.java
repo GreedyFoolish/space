6312a3fec4ee1c1ddfb38b095d523c216d66cff4
GreedyFoolish <1158984036@qq.com>
2025-05-12 21:04:56 +0800

    feat: 依赖优化

--- space/src/main/java/com/example/space/model/User.java
+++ space/src/main/java/com/example/space/model/User.java
@@ -1,9 +1,15 @@
 package com.example.space.model;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Size;
-import lombok.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 @Setter
