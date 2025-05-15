91b9df376476ade183d0a396cd78214896704e71
GreedyFoolish <1158984036@qq.com>
2025-05-15 13:38:15 +0800

    feat: element-plus自动导入css样式文件插件

--- space-ui/vite.config.js
+++ space-ui/vite.config.js
@@ -1,9 +1,10 @@
-import {defineConfig} from "vite"
-import vue from "@vitejs/plugin-vue"
 import path from "path"
+import vue from "@vitejs/plugin-vue"
 import AutoImport from "unplugin-auto-import/vite"
-import Components from "unplugin-vue-components/vite"
+import ElementPlusPlugin from "unplugin-element-plus/vite"
 import {ElementPlusResolver} from "unplugin-vue-components/resolvers"
+import Components from "unplugin-vue-components/vite"
+import {defineConfig} from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -15,6 +16,7 @@ export default defineConfig({
         Components({
             resolvers: [ElementPlusResolver()],
         }),
+        ElementPlusPlugin()
     ],
     resolve: {
         alias: {
