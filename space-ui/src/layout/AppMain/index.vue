92f59add6a83e9d7a421f5e5861b8249ee29a878
GreedyFoolish <1158984036@qq.com>
2025-06-04 15:56:38 +0800

    feat: 页面主体内容页面结构初始化

--- space-ui/src/layout/AppMain/index.vue
+++ space-ui/src/layout/AppMain/index.vue
@@ -1,11 +1,40 @@
 <template>
-
+    <section class="app-main">
+        <transition name="fade-transform" mode="out-in">
+            <keep-alive>
+                <router-view :key="key" />
+            </keep-alive>
+        </transition>
+    </section>
 </template>
 
 <script setup>
+import { ref, onBeforeMount } from "vue"
+import { useRoute } from "vue-router"
+import { useAppConfigStore } from "@/stores/appConfigStore.js"
 
+const appConfig = useAppConfigStore()
+const key = ref("")
+
+onBeforeMount(() => {
+    key.value = useRoute().path
+})
 </script>
 
 <style scoped>
+.fade-transform--move,
+.fade-transform-leave-active,
+.fade-transform-enter-active {
+    transition: all 0.5s;
+}
 
+.fade-transform-enter {
+    opacity: 0;
+    transform: translateX(-30px);
+}
+
+.fade-transform-leave-to {
+    opacity: 0;
+    transform: translateX(30px);
+}
 </style>
