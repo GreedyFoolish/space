36c4973774b0aa31c4ea1e6eea63331f1d05bd62
GreedyFoolish <1158984036@qq.com>
2025-07-03 16:47:19 +0800

    feat(components): 实现了一个基于 Element UI 的自定义分页组件 1、支持自定义每页显示条目数和数据总条目数 2、提供了分页变化时的事件回调

---
+++ space-ui/src/components/Pagination/index.vue
@@ -0,0 +1,99 @@
+<template>
+    <div v-show="!hidden" class="pagination-container">
+        <el-pagination
+            :background="background"
+            :current-page="currentPage"
+            :page-size="pageSize"
+            :page-sizes="pageSizes"
+            :layout="layout"
+            :total="total"
+            @current-change="handleCurrentChange"
+            @size-change="handleSizeChange"
+        >
+        </el-pagination>
+    </div>
+</template>
+
+<script setup>
+import { defineEmits } from "vue"
+import { smoothScrollTo } from "@/utils/scrollUtils.js"
+
+const props = defineProps({
+    // 是否显示
+    hidden: {
+        type: Boolean,
+        default: false
+    },
+    // 是否为分页按钮添加背景色
+    background: {
+        type: Boolean,
+        default: true
+    },
+    // 当前页码
+    currentPage: {
+        type: Number,
+        default: 1
+    },
+    // 每页显示条目个数
+    pageSize: {
+        type: Number,
+        default: 10
+    },
+    // 每页显示个数选择器的选项设置
+    pageSizes: {
+        type: Array,
+        default: () => [10, 20, 30, 50]
+    },
+    // 显示的分页按钮
+    layout: {
+        type: String,
+        default: "total, sizes, prev, pager, next, jumper"
+    },
+    // 数据总条目数
+    total: {
+        type: Number,
+        default: 0
+    },
+    // 是否自动滚动到顶部
+    autoScroll: {
+        type: Boolean,
+        default: true
+    }
+})
+
+const emits = defineEmits(["pagination", "update:currentPage", "update:pageSize"])
+
+const scrollToTopIfNecessary = () => {
+    if (props.autoScroll && window.scrollY > 0) {
+        smoothScrollTo({ to: 0, duration: 800 })
+    }
+}
+
+const handleCurrentChange = (value) => {
+    emits("pagination", { pageIndex: value, pageSize: props.pageSize })
+    emits("update:currentPage", value)
+    scrollToTopIfNecessary()
+}
+
+const handleSizeChange = (value) => {
+    const newPageSize = parseInt(value)
+    if (isNaN(newPageSize) || newPageSize <= 0) {
+        return
+    }
+
+    let newPage = props.currentPage
+    // 如果当前页的数据已不存在，则跳回第一页
+    if (newPage * newPageSize > props.total) {
+        newPage = 1
+        emits("update:currentPage", newPage)
+    }
+
+    emits("pagination", { pageIndex: newPage, pageSize: newPageSize })
+    emits("update:pageSize", newPageSize)
+    scrollToTopIfNecessary()
+}
+</script>
+
+<style scoped>
+
+</style>
